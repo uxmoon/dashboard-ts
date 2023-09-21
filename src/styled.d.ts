24a677ae83868f6132003df69e9693823f74242c
uxmoon <uxmoon@gmail.com>
2023-09-20 23:40:59 -0300

    Added definition file

---
+++ src/styled.d.ts
@@ -0,0 +1,28 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      body: string
+      bodyPattern: string
+      card: string
+      text: string
+      headings: string
+      chart: string
+      primary: {
+        green: string
+        red: string
+      }
+      social: {
+        facebook: string
+        twitter: string
+        instagram: string
+        youtube: string
+      }
+      toggle: {
+        default: string
+        active: string
+      }
+    }
+  }
+}
