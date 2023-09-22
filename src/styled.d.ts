f8748e7b040e02465c07ec9fdcb956f81981f7ed
uxmoon <uxmoon@gmail.com>
2023-09-22 11:51:33 -0300

    Add card hover color

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -6,6 +6,7 @@ declare module 'styled-components' {
       body: string
       bodyPattern: string
       card: string
+      cardHover: string
       text: string
       headings: string
       chart: string
